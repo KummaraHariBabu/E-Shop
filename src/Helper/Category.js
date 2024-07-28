71922691993ddd9095a7734881e7d7b403d48f0c
KummaraHariBabu <kummaraharibabu8@gmail.com>
2024-07-28 12:14:01 +0530

    made changes on category.js

--- src/Helper/Category.js
+++ src/Helper/Category.js
@@ -2,11 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Category(props) {
+    const {slug,name,url} = props.value;
     return (
         <div className="col-lg-3 col-md-4 col-sm-6 mt-2 mb-2 text-center">
             <div className="card">
                 <div className="card-body text-white text-wrap">
-                    <NavLink to={`/products/${props.value}`} className="btn">{props.value}</NavLink>
+                    <NavLink to={`/products/${slug}`} className="btn">{name}</NavLink>
                 {/* Here value is not built-in keyword.. */}
                 </div>
             </div>
